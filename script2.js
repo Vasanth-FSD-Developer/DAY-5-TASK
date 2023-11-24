ea03b97d2028e935889d205ab618821113231128
Vasanth-FSD-Developer <150881126+Vasanth-FSD-Developer@users.noreply.github.com>
2023-11-24 23:41:49 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,27 @@
+let resume = {
+    "name": "Vasanthakumar",
+
+    "contact": "8270402248",
+
+      "email": "vasanthkumarmba11@gmail.com",
+
+      "address": "P-102A North street sengipatti-613402",
+
+      "Degree" : "Master of Business Adminstration",
+
+        "institution": "PMIST unviresity",
+
+        "location": "Thanjavur",
+
+        "graduation_year": "2023",
+
+        "About Me": "An ambitous MBA gradute eager to learn new techonologies and i have excellent analytical skills",
+
+        "skills": "JavaScript,HTML,CSS,Python,promblem solving,Team work",
+
+        "certifications":"GUVI-FSD-IIT",
+
+        "Interst": "Web development"
+    }
+
+    console.log(resume);
